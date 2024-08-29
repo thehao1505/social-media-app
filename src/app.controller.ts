5e59fdb33de089cf24402149a85faa6d89f3722b
The Hao <nt.hao@tego.global>
2024-08-29 13:38:52 +0700

    Second commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return {
+      title: 'Dominate the world version',
+      status: 'alive',
+    };
   }
 }
