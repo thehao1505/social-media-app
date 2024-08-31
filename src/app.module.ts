0a293571858dcda5c3ea6f194b9190228038b48a
The Hao <nt.hao@tego.global>
2024-08-31 15:29:22 +0700

    Add users/auth/mail module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { UsersModule } from './modules/users/users.module';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        secure: false,
+        auth : {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+      },
+      defaults: {
+        from: `"No Reply" <no-reply@gmail.com>`,
+      },
+    })
+  ],
   controllers: [AppController],
-  providers: [],
 })
 export class AppModule {}
